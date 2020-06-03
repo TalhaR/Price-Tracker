765a645b53b3f4e5a52c24304a1efe503ebc2a66
TalhaR <talhar@github.com>
2020-06-02 22:33:19 -0400

    included imports

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import request
+from bs4 import BeautifulSoup
 
 
 def main():
-    print(1)
+    url = "https://talhar.github.io/"
+    page = request.GET(url)
+
+    soup = BeautifulSoup(page.content, 'html.paser')
 
 
 if __name__ == '__main__':
