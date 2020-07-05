ca8ba14a4d644c4ff5a59a2464986c79d907490d
TalhaR <talhar@github.com>
2020-07-05 11:36:42 -0400

    added config file for bot & User credentials

---
+++ config.py
@@ -0,0 +1,6 @@
+# These are not real credentials
+# To use, create your own gmail bot account and fill out
+# along with adding your own email address
+bot_email = 'BOT_EMAIL@gmail.com'
+bot_password = 'BOT_PASSWORD'
+recipient = 'YOUR_EMAIL@gmail.com'
